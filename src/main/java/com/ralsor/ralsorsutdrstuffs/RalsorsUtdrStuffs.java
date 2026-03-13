0d62de889f94b11e59e99c31a7567328414f051e
nine-nine-nine-9-9-9 <dendindonchik@gmail.com>
2026-03-13 18:27:52 +0300

    Mine carp

---
+++ src/main/java/com/ralsor/ralsorsutdrstuffs/RalsorsUtdrStuffs.java
@@ -0,0 +1,15 @@
+package com.ralsor.ralsorsutdrstuffs;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RalsorsUtdrStuffs implements ModInitializer {
+	public static final String MOD_ID = "ralsors-utdr-stuffs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/ralsor/ralsorsutdrstuffs/RalsorsUtdrStuffsClient.java
@@ -0,0 +1,10 @@
+package com.ralsor.ralsorsutdrstuffs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RalsorsUtdrStuffsClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
